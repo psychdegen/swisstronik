4cfa07607f7cc9601b894cf51badd75e27f064da
psychdegen <114776285+psychdegen@users.noreply.github.com>
2024-07-31 00:27:51 +0100

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,8 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/",
-      accounts: ["0x87b8dd08d1bcd7f01ac980462e18a94e04069a9f06e0097be54f5298bab8bd7c"], //Your private key starting with "0x"
+      accounts: ["0x
+                "], //Your private key starting with "0x"
     },
   },
 };
